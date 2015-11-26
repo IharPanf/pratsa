48dc35797a7c9e729d1ff376a059e1302661ee44
IharPanf <panfilenko.igor@gmail.com>
2015-11-26 18:27:51 +0200

    add routing

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,42 @@
-	$(document).ready(function(){
-		$('#btnContact').bind('click',function(){
-			 $('#modal').modal();
-		});
-		$('.grid-item').bind('click',function(){
+$('#btnContact').on('click',function(){
+	 $('#modal').modal();
+});
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 0) {
+		$('#up').fadeIn();	
+	} else {
+		$('#up').fadeOut();
+	}			
+});
+$('#up').on('click',function(){
+	$("body,html").animate({
+					"scrollTop" : 0
+					},"slow",function(){
+									$('#up').fadeOut();	
+								 });
+})
+
+//Отложенная загрузка, ждем загрузки  AngularJS		
+setTimeout(function(){
+		$('.grid-item').on('click',function(){
 			alert('В стадии разработки');
 		});
 		$('.grid').isotope({
 			itemSelector: '.grid-item',
 			layoutMode: 'fitRows'
 		});
-		$('.blockTag ul li').bind('click',function() {
+		$('.blockTag ul li').on('click',function() {
 							var curAttr = $(this).attr('data-tag');
+							console.log(curAttr);
 							$('.curTag').fadeOut(200,function(){
 								$('.curTag').text('#'+curAttr).fadeIn();		
 							});
+							console.log($('.grid'));
 							$('.grid').isotope({
-										filter: function() {
+										filter: function() {	
 													var tag = false;
 													var arrTags = $(this).find('.tag').text();
+													console.log($(this));
 													arrTags = arrTags.split('#');
 													$.each(arrTags,function(index,value){
 														if (value == curAttr) {
@@ -28,5 +47,6 @@
 													}	
 													
 												})
-							});			
-	});
+							});				
+		
+},1000);
